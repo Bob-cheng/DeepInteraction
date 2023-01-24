1ffa2b9378e3e78ac0d2289f13398c319c133400
bobchengzy <bobchengzy@gmail.com>
2023-01-24 04:17:58 -0500

    - move MyPad, MyNormalize, MyResize, MyFlip3D from plugin to main package

--- projects/mmdet3d_plugin/datasets/pipelines/__init__.py
+++ projects/mmdet3d_plugin/datasets/pipelines/__init__.py
@@ -3,10 +3,12 @@ from .transform_3d import (
     # PadMultiViewImage, NormalizeMultiviewImage, 
     # PhotoMetricDistortionMultiViewImage, 
     ScaleImageMultiViewImage,
-    MyPad, MyNormalize, MyResize, MyFlip3D, LoadMultiViewImageFromFilesWaymo)
+    # MyPad, MyNormalize, MyResize, MyFlip3D, 
+    LoadMultiViewImageFromFilesWaymo)
 
 __all__ = [
     # 'PadMultiViewImage', 'NormalizeMultiviewImage', 'PhotoMetricDistortionMultiViewImage', 
     'ScaleImageMultiViewImage',
-    'MyPad', 'MyNormalize', 'MyResize', 'LoadMultiViewImageFromFilesWaymo', 'MyFlip3D'
+    # 'MyPad', 'MyNormalize', 'MyResize', 'MyFlip3D', 
+    'LoadMultiViewImageFromFilesWaymo'
 ]
